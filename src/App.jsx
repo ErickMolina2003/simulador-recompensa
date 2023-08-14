d2f1d3e1571e2b43052341fb2e3c03629e9bf163
nikolas-oss <nicolastislovomontenegro@gmail.com>
2023-08-13 18:36:20 -0600

    Agregando Estadistica

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { Cases } from './components/Cases';
 import { CreateCase } from './components/CreateCase';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { OpenCase } from './components/OpenCase';
+import { EstadisticaObjeto } from './components/EstadisticaObjeto';
+import { EstadisticaCaja } from './components/EstadisticaCaja';
 
 function App() {
   return (
@@ -12,9 +14,11 @@ function App() {
       <Router>
         <NavBar />
         <Routes>
-          <Route path='/' Component={Cases}></Route>
-          <Route path='/crear-caja' exact Component={CreateCase}></Route>
-          <Route path='/abrir-caja' exact Component={OpenCase}></Route>
+          <Route path='/' element={<Cases />} />
+          <Route path='/crear-caja' element={<CreateCase />} />
+          <Route path='/abrir-caja' element={<OpenCase />} />
+          <Route path='/EstadisticaObjeto' element={<EstadisticaObjeto />} />
+          <Route path='/EstadisticaCaja' element={<EstadisticaCaja />} />
         </Routes>
       </Router>
     </main>
