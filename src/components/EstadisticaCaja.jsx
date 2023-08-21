a9b7c6f63bba80c7c649ae68c4080929836c4881
HanselHrz <hansel.hernandezc@gmail.com>
2023-08-21 15:28:18 -0600

    Estadistica Caja

--- src/components/EstadisticaCaja.jsx
+++ src/components/EstadisticaCaja.jsx
@@ -1,174 +1,239 @@
-import { Button, Col, Container, Row } from 'react-bootstrap';
-import Stadistics from '../assets/Stadistics.svg';
+import React, { useState } from "react";
+import {
+  Button,
+  Col,
+  Container,
+  Row,
+  Card,
+  FormControl,
+  Form,
+} from "react-bootstrap";
+import Stadistics from "../assets/Stadistics.svg";
 
 function EstadisticaCaja() {
+  const [cantidadObjetos, setCantidadObjetos] = useState("");
+  const [objetosEnCaja, setObjetosEnCaja] = useState([]);
+  const [probabilidades, setProbabilidades] = useState([]);
+  const [precios, setPrecios] = useState([]);
+  const [precioTotalCaja, setPrecioTotalCaja] = useState("");
+  const [numeroIntentos, setNumeroIntentos] = useState("");
+  const [retribuido, setRetribuido] = useState(0);
+
+  const handleCantidadChange = (event) => {
+    setCantidadObjetos(event.target.value);
+  };
+
+  const handleProbabilidadChange = (index, event) => {
+    const updatedProbabilidades = [...probabilidades];
+    updatedProbabilidades[index] = event.target.value;
+    setProbabilidades(updatedProbabilidades);
+  };
+
+  const handlePrecioChange = (index, event) => {
+    const updatedPrecios = [...precios];
+    updatedPrecios[index] = event.target.value;
+    setPrecios(updatedPrecios);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const objetos = [];
+    const nuevasProbabilidades = [];
+    const nuevosPrecios = [];
+
+    for (let i = 0; i < cantidadObjetos; i++) {
+      objetos.push(`Objeto ${i + 1}`);
+      nuevasProbabilidades.push("");
+      nuevosPrecios.push("");
+    }
+
+    setObjetosEnCaja(objetos);
+    setProbabilidades(nuevasProbabilidades);
+    setPrecios(nuevosPrecios);
+  };
+  const handlePrecioTotalCajaChange = (event) => {
+    setPrecioTotalCaja(event.target.value);
+  };
+
+  const handleNumeroIntentosChange = (event) => {
+    setNumeroIntentos(event.target.value);
+  };
+
+  const handleCalcularClick = () => {
+    let retribuido = 0;
+    for (let i = 0; i < numeroIntentos; i++) {
+      const randomValue = Math.random();
+      let cumulativeProbability = 0;
+      for (let j = 0; j < probabilidades.length; j++) {
+        cumulativeProbability += parseFloat(probabilidades[j]);
+        if (randomValue <= cumulativeProbability) {
+          retribuido += parseFloat(precios[j]);
+          break;
+        }
+      }
+    }
+    setRetribuido(retribuido);
+  };
+  const margenGanancia =
+    ((1 - retribuido / (precioTotalCaja * numeroIntentos)) * 100).toFixed(2) |
+    0;
+
   return (
-    <Container fluid className='cases-container'>
-      <Row className='my-3'>
-        <Col className='d-flex'>
+    <Container fluid className="cases-container">
+      <Row className="my-3 mx-3">
+        <Col className="d-flex">
           <img
             src={Stadistics}
-            alt='resumen'
+            alt="resumen"
             width={25}
             height={30}
-            className='me-2'
+            className="me-2"
           />
-          <h3 className='text-white'>ESTADISTICA CAJAS</h3>
+          <h3 className="text-white">ESTADISTICA CAJAS</h3>
         </Col>
       </Row>
-
-      <Col sm={'auto'} md={'auto'} lg={'auto'}>
-        <Row>
-          <Col>
-            <h4 className='text-white text-center'>
-              DETERMINAR MARGEN DE VALOR DE UNA CAJA
-            </h4>
-          </Col>
-          <Col></Col>
-        </Row>
-      </Col>
-      <br></br>
-      <br></br>
-      <Row>
-        <Col>
-          <h5 className='text-white text-center'>
-            CANTIDAD DE OBJETOS EN CAJA
-          </h5>
-        </Col>
-
-        <Col>
-          <h5 className='text-white text-center'>DATOS DE LOS OBJETOS</h5>
-        </Col>
-      </Row>
-      <br></br>
-
-      <Row>
-        <Col></Col>
-        <Col className='d-flex justify-content-center'>
-          <input></input>
-        </Col>
-        <Col></Col>
-        <Col>
-          <h6 className='text-white text-center'>LISTA DE OBJETOS</h6>
-        </Col>
-        <Col>
-          <h6 className='text-white text-center'>PROBABILIDAD OCURRENCIA</h6>
-        </Col>
-        <Col>
-          <h6 className='text-white text-center'>PRECIO </h6>
-        </Col>
-      </Row>
-      <br></br>
-
-      <Row>
-        <Col>
-          <h6 className='text-white text-center'>PRECIO DE LA CAJA </h6>
-        </Col>
-
-        <Col></Col>
-      </Row>
-      <Row>
-        <Col></Col>
-        <Col className='d-flex justify-content-center'>
-          <input></input>
-        </Col>
-        <Col></Col>
-        <Col>
-          <h6 className='text-white text-center'>OBJETO A:</h6>
-        </Col>
-        <Col className='d-flex justify-content-center'>
-          <input></input>
-        </Col>
-        <Col className='d-flex justify-content-center'>
-          <input></input>
-        </Col>
-        <br></br>
-      </Row>
-      <br></br>
-      <Row>
-        <Col></Col>
-        <Col>
-          <h6 className='text-white text-center'>NUMERO DE INTENTOS</h6>
-        </Col>
-        <Col></Col>
-        <Col>
-          <h6 className='text-white text-center'>OBJETO B:</h6>
-        </Col>
-        <Col className='d-flex justify-content-center'>
-          <input></input>
-        </Col>
-        <Col className='d-flex justify-content-center'>
-          <input></input>
-        </Col>
-        <br></br>
-      </Row>
-      <br></br>
-      <Row>
-        <Col></Col>
-        <Col className='d-flex justify-content-center'>
-          {' '}
-          <input></input>
-        </Col>
-
-        <Col></Col>
-
-        <Col>
-          <h6 className='text-white text-center'>OBJETO C:</h6>
-        </Col>
-        <Col className='d-flex justify-content-center'>
-          <input></input>
-        </Col>
-        <Col className='d-flex justify-content-center'>
-          <input></input>
-        </Col>
-        <br></br>
-      </Row>
-      <br></br>
-      <Row>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col>
-          <h6 className='text-white text-center'>OBJETO D:</h6>
-        </Col>
-        <Col className='d-flex justify-content-center'>
-          <input></input>
-        </Col>
-        <Col className='d-flex justify-content-center'>
-          <input></input>
-        </Col>
-        <br></br>
-      </Row>
-      <br></br>
-      <br></br>
-
-      <Row>
-        <Col>
-          <h5 className='text-white text-center'>
-            OBTENIDO: <input></input>
-          </h5>
-        </Col>
-        <Col className='d-flex justify-content-center'>
-          <Button variant='danger'>SIMULAR</Button>
-        </Col>
-      </Row>
-      <br></br>
-      <Row>
-        <Col>
-          <h5 className='text-white text-center'>
-            RETRIBUIDO: <input></input>
-          </h5>
-        </Col>
-        <Col></Col>
-      </Row>
-      <br></br>
-      <Row>
-        <Col>
-          <h5 className='text-white text-center'>
-            MARGEN DE GANANCIA: <input></input>
-          </h5>
-        </Col>
-        <Col></Col>
+      <Row className="my-1 mx-3">
+        <Card
+          style={{
+            width: "100rem",
+            height: "37rem",
+            backgroundColor: "rgba(255, 255, 255, 0.7)",
+          }}
+        >
+          <Card.Title className="mt-3">
+            <strong>DETERMINAR MARGEN DE VALOR DE UNA CAJA</strong>
+          </Card.Title>
+          <hr />
+          <Row className="mx-1">
+            <Col sm={4}>
+              <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="cantidad">
+                  <Form.Label style={{ fontSize: "1.3rem" }}>
+                    Cantidad de objetos en caja
+                  </Form.Label>
+                  <FormControl
+                    type="number"
+                    min={0}
+                    max={9}
+                    value={cantidadObjetos}
+                    placeholder="0 - 9"
+                    onChange={handleCantidadChange}
+                  />
+                </Form.Group>
+              </Form>
+              <Form>
+                <Form.Group controlId="precioTotal">
+                  <Form.Label style={{ fontSize: "1.3rem" }}>
+                    Precio Total de Caja
+                  </Form.Label>
+                  <FormControl
+                    type="number"
+                    min={0}
+                    value={precioTotalCaja}
+                    placeholder="0 LPS"
+                    onChange={handlePrecioTotalCajaChange}
+                  />
+                </Form.Group>
+                <Form.Group controlId="intentos">
+                  <Form.Label style={{ fontSize: "1.3rem" }}>
+                    Número de Intentos
+                  </Form.Label>
+                  <FormControl
+                    type="number"
+                    min={0}
+                    placeholder="0"
+                    value={numeroIntentos}
+                    onChange={handleNumeroIntentosChange}
+                  />
+                </Form.Group>
+              </Form>
+            </Col>
+            <Col sm={8}>
+              <h5 className="text-center">DATOS DE OBJETOS</h5>
+              <Row>
+                <Col>
+                  <h5 className="text-center">Lista de objetos</h5>
+                  <p style={{ fontSize: "1.3rem" }}>
+                    {objetosEnCaja.map((objeto, index) => (
+                      <li key={index}>{objeto}</li>
+                    ))}
+                  </p>
+                </Col>
+                <Col>
+                  <h5 className="text-center">Probabilidad ocurrencia</h5>
+                  {probabilidades.map((probabilidad, index) => (
+                    <Form.Control
+                      key={index}
+                      type="number"
+                      min={0}
+                      max={1}
+                      value={probabilidad}
+                      onChange={(event) =>
+                        handleProbabilidadChange(index, event)
+                      }
+                      placeholder="0 - 1"
+                      size="sm"
+                    />
+                  ))}
+                </Col>
+                <Col>
+                  <h5 className="text-center">Precio</h5>
+                  {precios.map((precio, index) => (
+                    <Form.Control
+                      key={index}
+                      type="number"
+                      value={precio}
+                      min={0}
+                      placeholder="0 LPS"
+                      onChange={(event) => handlePrecioChange(index, event)}
+                      size="sm"
+                    />
+                  ))}
+                </Col>
+              </Row>
+              <Row className="mt-1">
+                <Col>
+                  <Button
+                    variant="primary"
+                    type="button"
+                    onClick={handleCalcularClick}
+                  >
+                    Retribuido / Margen de Ganancia
+                  </Button>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <Row className="my-1 mx-3">
+            <hr />
+            <Col>
+              <h5>
+                <strong>RESULTADOS</strong>
+              </h5>
+            </Col>
+            <hr />
+            <Row>
+              <Col>
+                <h5 className="text-center">Obtenido</h5>
+                <div className="text-center bg-white">
+                  {precioTotalCaja * numeroIntentos + " LPS"}
+                </div>
+              </Col>
+              <Col>
+                <h5 className="text-center">Retribuido</h5>
+                <div className="text-center bg-white">
+                  {retribuido.toFixed(2) + " LPS"}
+                </div>
+              </Col>
+              <Col>
+                <h5>Margen de Ganancia</h5>
+                <div className="text-center bg-white">
+                  {margenGanancia + " %"}
+                </div>
+              </Col>
+            </Row>
+          </Row>
+        </Card>
       </Row>
     </Container>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,50 +1,50 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import CaseLogo from '../assets/logo.svg';
-import StadisticsLogo from '../assets/stadistics.svg';
-import BoxLogo from '../assets/case.svg';
-import InfoLogo from '../assets/info.svg';
-import { Link } from 'react-router-dom';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import CaseLogo from "../assets/logo.svg";
+import StadisticsLogo from "../assets/stadistics2.svg";
+import BoxLogo from "../assets/case.svg";
+import InfoLogo from "../assets/info.svg";
+import { Link } from "react-router-dom";
 
 export function NavBar() {
   return (
-    <Navbar expand='lg'>
+    <Navbar expand="lg">
       <Container fluid>
-        <Navbar.Brand as={Link} to='/'>
-          <img src={CaseLogo} width={200} alt='Simulador de Recompensa'></img>
+        <Navbar.Brand as={Link} to="/">
+          <img src={CaseLogo} width={200} alt="Simulador de Recompensa"></img>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls='navbarScroll' />
-        <Navbar.Collapse id='navbarScroll' className='justify-content-end'>
-          <Nav style={{ maxHeight: '100px' }} navbarScroll>
-            <Nav.Link as={Link} to='/'>
-              <img src={BoxLogo} alt='caja' width={30} height={15} />
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll" className="justify-content-end">
+          <Nav style={{ maxHeight: "100px" }} navbarScroll>
+            <Nav.Link as={Link} to="/">
+              <img src={BoxLogo} alt="caja" width={30} height={15} />
               Cajas
             </Nav.Link>
-            <Nav.Link as={Link} to='/crear-caja'>
-              <img src={BoxLogo} alt='caja' width={30} height={15} />
+            <Nav.Link as={Link} to="/crear-caja">
+              <img src={BoxLogo} alt="caja" width={30} height={15} />
               Crear caja
             </Nav.Link>
-            <Nav.Link as={Link} to='/estadistica-objeto'>
+            <Nav.Link as={Link} to="/estadistica-objeto">
               <img
                 src={StadisticsLogo}
-                alt='estadisticas'
+                alt="estadisticas"
                 width={30}
                 height={15}
               />
               Estadística Objetos
             </Nav.Link>
-            <Nav.Link as={Link} to='/estadistica-caja'>
+            <Nav.Link as={Link} to="/estadistica-caja">
               <img
                 src={StadisticsLogo}
-                alt='estadisticas'
+                alt="estadisticas"
                 width={30}
                 height={15}
               />
               Estadística Caja
             </Nav.Link>
-            <Nav.Link as={Link} to='/como-funciona'>
-              <img src={InfoLogo} alt='funcionamiento' width={30} height={15} />
+            <Nav.Link as={Link} to="/como-funciona">
+              <img src={InfoLogo} alt="funcionamiento" width={30} height={15} />
               Cómo funciona
             </Nav.Link>
           </Nav>
